65e8f634dc865f9b11e944619558c6096e916123
Arjay Angeles <aqangeles@gmail.com>
2017-06-30 18:01:06 +0800

    Remove unnecessary conditions. Always process the output provided as is.

--- src/Transformers/FractalTransformer.php
+++ src/Transformers/FractalTransformer.php
@@ -27,33 +27,18 @@ class FractalTransformer
     /**
      * Transform output using the given transformer and serializer.
      *
-     * @param array $output
+     * @param mixed $output
      * @param mixed $transformer
      * @param mixed $serializer
      * @return array
      */
-    public function transform(array $output, $transformer, $serializer = null)
+    public function transform($output, $transformer, $serializer = null)
     {
         if ($serializer !== null) {
             $this->fractal->setSerializer($this->createSerializer($serializer));
         }
 
-        //Get transformer reflection
-        //Firs method parameter should be data/object to transform
-        $reflection = new \ReflectionMethod($transformer, 'transform');
-        $parameter  = $reflection->getParameters()[0];
-
-        //If parameter is class assuming it requires object
-        //Else just pass array by default
-        if ($parameter->getClass()) {
-            $resource = new Collection($output, $this->createTransformer($transformer));
-        } else {
-            $resource = new Collection(
-                $output,
-                $this->createTransformer($transformer)
-            );
-        }
-
+        $resource   = new Collection($output, $this->createTransformer($transformer));
         $collection = $this->fractal->createData($resource)->toArray();
 
         return $collection['data'];
