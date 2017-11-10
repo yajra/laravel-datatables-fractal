42d8707b59be655735548b6cd6953766f0d91f3b
ildiko <ildiko.kiszlinger@paytech.hu>
2017-11-10 14:25:47 +0100

    Updating an earlier pull request: https://github.com/yajra/laravel-datatables/pull/838

--- src/Transformers/FractalTransformer.php
+++ src/Transformers/FractalTransformer.php
@@ -38,10 +38,22 @@ class FractalTransformer
             $this->fractal->setSerializer($this->createSerializer($serializer));
         }
 
-        $resource   = new Collection($output, $this->createTransformer($transformer));
-        $collection = $this->fractal->createData($resource)->toArray();
+        $collector = [];
+        foreach ($transformer as $transform) {
+            if ($transform != null) {
+                $resource = new Collection($output, $this->createTransformer($transform));
+                $collection = $this->fractal->createData($resource)->toArray();
+                $transformed = $collection['data'];
+                $collector = array_map(function ($item_collector, $item_transformed) {
+                    if ($item_collector === null) {
+                        $item_collector = [];
+                    }
+                    return array_merge($item_collector, $item_transformed);
+                }, $collector, $transformed);
+            }
+        }
 
-        return $collection['data'];
+        return $collector;
     }
 
     /**
